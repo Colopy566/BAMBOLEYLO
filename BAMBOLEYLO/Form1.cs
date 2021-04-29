a757d590a2d59d7fb9c6abfb4f3a04e65f6f0442
Colopy <sawa@SAWA->
2021-04-29 20:03:50 +0300

    Добавьте файлы проекта.

---
+++ BAMBOLEYLO/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BamboleyLO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Text = "НАЧАТЬ";
+            this.Text = "Угадай число";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Компьютер загадывает число от 1 до 100," +
+                " а вы пытаетесь его угадать за минимальное число попыток");
+            var f = new Form2();
+            f.Show();
+
+        }
+    }
+}
---
+++ BAMBOLEYLO/Form2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace BamboleyLO
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            this.Text = "Угадай число";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            Random rnd = new Random();
+            int value = rnd.Next(1, 100);
+            if (e.KeyChar != 8 && (e.KeyChar < 48 || e.KeyChar > 57))
+                e.Handled = true;
+            if (e.KeyChar == value)
+            {
+                label1.Text="Поздравляю ,ты угадал";
+            }
+            else label1.Text = "Попробуй еще раз";
+        }
+    }
+}
